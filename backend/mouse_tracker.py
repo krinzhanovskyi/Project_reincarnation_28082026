a71541fac2810d2841262d488bf8625d3f5dc0c4
krinzhanovskyi <karatelkros@gmail.com>
2026-09-23 08:15:25 +0200

    cleaning all the code

--- backend/mouse_tracker.py
+++ backend/mouse_tracker.py
@@ -4,7 +4,6 @@ from PyQt6.QtGui import QCursor
 from config import HOVER_DELAY
 
 class MouseTracker(QObject):
-    # Теперь трекер сам генерирует безопасные PyQt-сигналы
     hover = pyqtSignal(int, int)
     move = pyqtSignal()
 
@@ -13,33 +12,28 @@ class MouseTracker(QObject):
         self.last_pos = (0, 0)
         self.last_time = time.time()
         self.is_hovering = False
-        
-        # Встроенный таймер PyQt (работает в главном потоке, без конфликтов)
         self.timer = QTimer()
         self.timer.timeout.connect(self._check_mouse)
         
     def start(self):
         self.last_time = time.time()
-        self.timer.start(50)  # Проверяем мышь 20 раз в секунду
+        self.timer.start(50)
 
     def stop(self):
         self.timer.stop()
 
     def _check_mouse(self):
-        # Получаем глобальные координаты курсора на всем экране
         pos = QCursor.pos()
         current_pos = (pos.x(), pos.y())
         
         if current_pos != self.last_pos:
-            # Если мышь сдвинулась
             self.last_pos = current_pos
             self.last_time = time.time()
             
             if self.is_hovering:
                 self.is_hovering = False
-                self.move.emit() # Даем сигнал спрятать окно
+                self.move.emit()
         else:
-            # Если мышь стоит на месте
             if not self.is_hovering and (time.time() - self.last_time) >= HOVER_DELAY:
                 self.is_hovering = True
                 self.hover.emit(current_pos[0], current_pos[1])
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from PyQt6.QtWidgets import QApplication
 from backend.mouse_tracker import MouseTracker
 from ui.pocket_window import PocketWindow
-from backend.win_api import get_desktop_folder_under_cursor  # <-- Новый импорт
+from backend.win_api import get_desktop_folder_under_cursor
 
 def main():
     app = QApplication(sys.argv)
@@ -10,18 +10,14 @@ def main():
     
     pocket = PocketWindow()
     tracker = MouseTracker()
-    
-    # Теперь трекер вызывает нашу проверку, а не окно напрямую
+
     def handle_hover(x, y):
-        # Спрашиваем Windows, есть ли тут папка на рабочем столе
         folder_path = get_desktop_folder_under_cursor(x, y)
         
         if folder_path:
             print(f"[*] Найдена папка: {folder_path}")
-            # Пока просто открываем окно, в будущем передадим туда файлы
             pocket.show_at(x, y)
         else:
-            # Если папки нет (пустое место или другая программа), окно не появится
             pass
 
     tracker.hover.connect(handle_hover)
